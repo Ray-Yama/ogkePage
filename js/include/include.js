a9025f631cc49a11ed406a6ba384546846166ca8
Ray-Yama <115804690+Ray-Yama@users.noreply.github.com>
2022-10-15 17:51:28 +0900

    Update include.js

--- js/include/include.js
+++ js/include/include.js
@@ -1,5 +1,17 @@
 //共通パーツ読み込み
 $(function () {
+  var file_path;
+  var scripts = document.getElementsByTagName("script");
+  var i = scripts.length;
+  
+  while (i--) {
+    var match = scripts[i].src.match(/(^|.*\/)left_menu\.js$/);
+    if (match) {
+        file_path = match[1];
+        break;
+    }
+  }
+  
   $("#header").load("../page/include/header.html");
   //$("#side").load("side.html");
   //$("#footer").load("footer.html");
