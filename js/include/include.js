f35ecf7bfc6174d0c597367265b66b782ee2f3af
Ray-Yama <115804690+Ray-Yama@users.noreply.github.com>
2022-10-15 23:55:00 +0900

    Update include.js

--- js/include/include.js
+++ js/include/include.js
@@ -12,15 +12,15 @@ $(function () {
   //$("#side").load(pPath + "page/include/side.html");
   //$("#footer").load(pPath + "page/include/footer.html");
 
-	$(".openbtn").click(function () {//ボタンがクリックされたら
-		$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-		$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-	});
+　$(".openbtn").click(function () {//ボタンがクリックされたら
+　　$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+　　$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+　});
 
-	$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-		$(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-		$("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-	});
+　$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+　　$(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+　　$("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+　});
 });
 
 function showClock() {
