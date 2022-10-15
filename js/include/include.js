7a037377ca350ee02bd5a29fc55f2b51735d1665
Ray-Yama <115804690+Ray-Yama@users.noreply.github.com>
2022-10-15 23:45:47 +0900

    Update include.js

--- js/include/include.js
+++ js/include/include.js
@@ -13,6 +13,16 @@ $(function () {
   //$("#footer").load(pPath + "page/include/footer.html");
 });
 
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
 function showClock() {
    var nowTime = new Date();
    var nowHour = nowTime.getHours().toString().padStart(2, '0');
