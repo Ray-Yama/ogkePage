a43da8db238c082b860f9ab4dcf8c0355ec8931d
Ray-Yama <115804690+Ray-Yama@users.noreply.github.com>
2022-10-15 17:59:10 +0900

    Update include.js

--- js/include/include.js
+++ js/include/include.js
@@ -1,16 +1,9 @@
 //共通パーツ読み込み
 $(function () {
-  var file_path;
-  var scripts = document.getElementsByTagName("script");
-  var i = scripts.length;
-  
-  while (i--) {
-    var match = scripts[i].src.match(/(^|.*\/)left_menu\.js$/);
-    if (match) {
-        file_path = match[1];
-        break;
-    }
-  }
+  let url = location.href;
+  if (url == "https://ray-yama.github.io/ogkePage/") {
+    // URLが http://hoge.com/ の場合に実行する処理 
+  } 
   
   $("#header").load("../page/include/header.html");
   //$("#side").load("side.html");
