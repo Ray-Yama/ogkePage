c7abec6d5d12c464d26ad7ebe04a598c8180358d
Ray-Yama <115804690+Ray-Yama@users.noreply.github.com>
2022-10-15 19:39:35 +0900

    Update include.js

--- js/include/include.js
+++ js/include/include.js
@@ -12,3 +12,13 @@ $(function () {
   //$("#side").load(pPath + "page/include/side.html");
   //$("#footer").load(pPath + "page/include/footer.html");
 });
+
+function showClock1() {
+   var nowTime = new Date();
+   var nowHour = nowTime.getHours();
+   var nowMin  = nowTime.getMinutes();
+   var nowSec  = nowTime.getSeconds();
+   var msg = nowHour + ":" + nowMin + ":" + nowSec;
+   document.getElementById("txtClock").innerHTML = msg;
+}
+setInterval('showClock1()',1000);
